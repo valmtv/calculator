5f5e794e8bc54c9cd3b2f76a619c4f075ef67157
valmtv <valerij.matviiv@gmail.com>
2019-09-09 17:23:04 +0300

    master - cutback the area of code

--- src/app/index.js
+++ src/app/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import {Num9, Num8, Num7, Num6, Num5, Num4, Num3,
-Num2, Num1, Num0, Percent, Point
+import {
+  Num, Percent, Dot,
 } from '../button';
 
 const Body= styled.body`
@@ -11,23 +11,14 @@ const Body= styled.body`
   grid-template-areas: "num7 num8 num9"
                        "num6 num5 num4"
                        "num3 num2 num1"
-                       "percent num0 point";
+                       "percent num0 dot";
 `;
 
 const App = () => (
   <Body>
-    <Num9 />
-    <Num8 />
-    <Num7 />
-    <Num6 />
-    <Num5 />
-    <Num4 />
-    <Num3 />
-    <Num2 />
-    <Num1 />
-    <Num0 />
+    {[0,1,2,3,4,5,6,7,8,9].map(n => <Num value={n} key={n} />)}
     <Percent />
-    <Point />
+    <Dot />
   </Body>
 );
 export default App;
--- src/button/index.js
+++ src/button/index.js
@@ -2,106 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+export const Num = ({value}) => {
+  const conf = {
+    "0": "num0",
+    "1": "num1",
+    "2": "num2",
+    "3": "num3",
+    "4": "num4",
+    "5": "num5",
+    "6": "num6",
+    "7": "num7",
+    "8": "num8",
+    "9": "num9",
+  };
+  
+  const N9 = styled.div`
+    color: gray;
+    background-color: lightyellow;
+    grid-area: ${conf[value]};
+  `;
 
-const N9 = styled.div`
-  color: gray;
-  background-color: lightyellow;
-  grid-area: num9;
-`;
-
-export const Num9 = () => (
-  <N9>9</N9>
-); 
-
-const N8 = styled.div`
-  color: gray;
-  background-color: lightyellow;
-  grid-area: num8;
-`;
-
-export const Num8 = () => (
-  <N8>8</N8>
-); 
-
-const N7 = styled.div`
-  color: gray;
-  background-color: lightyellow;
-  grid-area: num7;
-`;
-
-export const Num7 = () => (
-  <N7>7</N7>
-); 
-
-const N6 = styled.div`
-  color: gray;
-  background-color: lightyellow;
-  grid-area: num6;
-`;
-
-export const Num6 = () => (
-  <N6>6</N6>
-); 
-
-const N5 = styled.div`
-  color: gray;
-  background-color: lightyellow;
-  grid-area: num5;
-`;
-
-export const Num5 = () => (
-  <N5>5</N5>
-); 
-
-const N4 = styled.div`
-  color: gray;
-  background-color: lightyellow;
-  grid-area: num4;
-`;
-
-export const Num4 = () => (
-  <N4>4</N4>
-); 
-
-const N3 = styled.div`
-  color: gray;
-  background-color: lightyellow;
-  grid-area: num3;
-`;
-
-export const Num3 = () => (
-  <N3>3</N3>
-); 
-
-const N2 = styled.div`
-  color: gray;
-  background-color: lightyellow;
-  grid-area: num2;
-`;
-
-export const Num2 = () => (
-  <N2>2</N2>
-); 
-
-const N1 = styled.div`
-  color: gray;
-  background-color: lightyellow;
-  grid-area: num1;
-`;
-
-export const Num1 = () => (
-  <N1>1</N1>
-); 
-
-const N0 = styled.div`
-  color: gray;
-  background-color: lightyellow;
-  grid-area: num0;
-`;
-
-export const Num0 = () => (
-  <N0>0</N0>
-); 
+  return (
+    <N9>{value}</N9>
+  ); 
+};
 
 const Perc = styled.div`
   color: gray;
@@ -113,12 +37,12 @@ export const Percent = () => (
   <Perc>%</Perc>
 ); 
 
-const point1 = styled.div`
+const Point = styled.div`
   color: gray;
   background-color: lightyellow;
-  grid-area: point;
+  grid-area: dot;
 `;
 
-export const Point = () => (
-  <point1>.</point1>
+export const Dot = () => (
+  <Point>.</Point>
 );
