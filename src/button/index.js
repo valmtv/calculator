b76c4b8b7b1cd082e653882de8bf94d1adbce434
valmtv <valerij.matviiv@gmail.com>
2020-02-10 17:13:31 +0200

    master - added all num, and placed "grid"

--- src/button/index.js
+++ src/button/index.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import {
-  Num1
+  Num1, Num2, Num3, Num4, Num5,
+  Num6, Num7, Num8, Num9, Num0
 } from './num';
 
 const button = styled.div`
@@ -13,12 +14,26 @@ const button = styled.div`
 `;
 const Div = styled.div`
   background-color: black;
+  display: grid;
+  grid-template-areas: "Num9 Num8 Num7";
+                       "Num6 Num5 Num4";
+                       "Num3 Num2 Num1";
+                       "Num0 Num0 Num0";
 `;
 
 export const Button = () => (
   <button>
     <Div>
+     <Num9/>
+     <Num8/>
+     <Num7/>
+     <Num6/>
+     <Num5/>
+     <Num4/>
+     <Num3/>
+     <Num2/>
      <Num1/>
+     <Num0/>
     </Div>
   </button>
 );
--- src/button/num/index.js
+++ src/button/num/index.js
@@ -2,13 +2,50 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const Num= styled.nav`
-  background-color: light-yellow;
+const Num = styled.nav`
+  background-color: white;
   color: black;
   font-size: 50px;  
+  padding-right: 50px;
 `;
+
 
 export const Num1 = () => (
   <Num>1</Num>
 );
 
+export const Num2 = () => (
+  <Num>2</Num>
+);
+
+export const Num3 = () => (
+  <Num>3</Num>
+);
+
+export const Num4 = () => (
+  <Num>4</Num>
+);
+
+export const Num5 = () => (
+  <Num>5</Num>
+);
+
+export const Num6 = () => (
+  <Num>6</Num>
+);
+
+export const Num7 = () => (
+  <Num>7</Num>
+);
+
+export const Num8 = () => (
+  <Num>8</Num>
+);
+
+export const Num9 = () => (
+  <Num>9</Num>
+);
+
+export const Num0 = () => (
+  <Num>0</Num>
+);
