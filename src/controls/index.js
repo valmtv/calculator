66ae0a5221383147ee38e968e6da5444cf38b875
valmtv <valerij.matviiv@gmail.com>
2020-02-20 17:59:17 +0200

    master - add value to operation

--- src/controls/index.js
+++ src/controls/index.js
@@ -2,38 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import {
-  Opr1, Opr2, Opr3, Opr4, Opr5,
-  Opr6, Opr7, Opr8, Equal
-} from './operation'
+import { Opr } from './operation'
 
 import { Num } from './num';
 
 const Div = styled.div`
   background-color: black;
   display: grid;
-  grid-template-areas: 'Opr8 Opr7 Opr6 Opr5'
-                       'Num9 Num8 Num7 Opr4'
-                       'Num6 Num5 Num4 Opr3'
-                       'Num3 Num2 Num1 Equal'
-                       'Opr1 Num0 Opr2 Equal';
+  grid-template-areas: 'cancel  divide multi delete'
+                       'Num9    Num8   Num7  minus'
+                       'Num6    Num5   Num4  plus'
+                       'Num3    Num2   Num1  equal'
+                       'percent Num0   point equal';
 `;
 
 export const Controls = () => (
   <Div>
-    <Opr7/>
-    <Opr6/>
-    <Opr5/>
-    <Opr4/>
-    <Opr3/>
-    <Equal/>
-    <Opr1/>
-    <Opr8/>
+    <Opr value='/' name='divide'/>
+    <Opr value='+' name='plus'/>
+    <Opr value='=' name='equal'/>
+    <Opr value='-' name='minus'/>
+    <Opr value='%' name='percent'/>
+    <Opr value='Del' name='delete'/>
+    <Opr value='X' name='multi'/>
+    <Opr value='C' name='cancel'/>
+    <Opr value=',' name='point'/>
     <Num name='0' />
     <Num name='1' />
     <Num name='2' />
     <Num name='3' />
-    <Opr2/>
     <Num name='4' />
     <Num name='5' />
     <Num name='6' />
--- src/controls/operation/index.js
+++ src/controls/operation/index.js
@@ -6,52 +6,13 @@ const Operation = styled.button`
   background-color: white;
   color: black;
   font-size: 50px;  
+  grid-area: ${ props => props.name };
   display: flex;
   justify-content: center;
 `;
 
-const Equal1 = styled.button`
-  background-color: white;
-  color: black;
-  font-size: 50px;  
-  display: flex;
-  justify-content: center;
-`;
-
-export const Opr1 = () => (
-  <Operation>%</Operation>
+export const Opr = ({value, name}) => (
+  <Operation name={name}>
+    {value}
+  </Operation>
 );
-
-export const Opr2 = () => (
-
-  <Operation>,</Operation>
-);
-
-export const Opr3 = () => (
-  <Operation>+</Operation>
-);
-
-export const Opr4 = () => (
-  <Operation>-</Operation>
-);
-
-export const Opr5 = () => (
-  <Operation>del</Operation>
-);
-
-export const Opr6 = () => (
-  <Operation>X</Operation>
-);
-
-export const Opr7 = () => (
-  <Operation>/</Operation>
-);
-
-export const Opr8 = () => (
-  <Operation>C</Operation>
-);
-
-export const Equal = () => (
-  <Equal1>=</Equal1>
-);
-
